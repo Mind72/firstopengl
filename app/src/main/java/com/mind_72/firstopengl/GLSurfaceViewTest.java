dc2b88d6e1346cab1939282e539b907a95a60caf
Mind72 <mvanminderhout@mind-72.com>
2017-10-04 07:27:24 +0200

    initial

---
+++ app/src/main/java/com/mind_72/firstopengl/GLSurfaceViewTest.java
@@ -0,0 +1,15 @@
+
+package com.mind_72.firstopengl;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GLSurfaceViewTest extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glsurface_view_test);
+    }
+}
